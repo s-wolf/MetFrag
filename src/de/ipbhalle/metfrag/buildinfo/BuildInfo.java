963f9384fa97b97245e51825eec0129e73fc09a6
Sebastian Wolf <swolf@ipb-halle.de>
2010-03-23 16:42:45 +0100

    successfully imported in eclipse

--- src/de/ipbhalle/metfrag/buildinfo/BuildInfo.java
+++ src/de/ipbhalle/metfrag/buildinfo/BuildInfo.java
@@ -2,7 +2,7 @@
 		package de.ipbhalle.metfrag.buildinfo;
 		public class BuildInfo {
 			public static final String revisionNumber="exported";
-			public static final String timeStamp="Tue, 23-March-2010 15:54:05 CET";
+			public static final String timeStamp="Tue, 23-March-2010 16:23:22 CET";
 			public static final String svnLog="svn: '.' is not a working copy";
 		}
 	
