4252ae5532dadb73fca3e374490d6b084b003c3e
Sebastian Wolf <swolf@ipb-halle.de>
2010-03-30 14:09:10 +0200

    minor fixes

--- src/de/ipbhalle/metfrag/buildinfo/BuildInfo.java
+++ src/de/ipbhalle/metfrag/buildinfo/BuildInfo.java
@@ -2,7 +2,7 @@
 		package de.ipbhalle.metfrag.buildinfo;
 		public class BuildInfo {
 			public static final String revisionNumber="exported";
-			public static final String timeStamp="Mon, 29-March-2010 16:33:14 CEST";
+			public static final String timeStamp="Tue, 30-March-2010 10:32:49 CEST";
 			public static final String svnLog="svn: '.' is not a working copy";
 		}
 	
