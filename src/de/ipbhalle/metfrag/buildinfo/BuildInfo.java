c37f7b39040d8a41833ecd76e50d41762097547a
Sebastian Wolf <swolf@ipb-halle.de>
2010-04-08 14:43:49 +0200

    Removed unneccessary file

--- src/de/ipbhalle/metfrag/buildinfo/BuildInfo.java
+++ src/de/ipbhalle/metfrag/buildinfo/BuildInfo.java
@@ -2,7 +2,7 @@
 		package de.ipbhalle.metfrag.buildinfo;
 		public class BuildInfo {
 			public static final String revisionNumber="exported";
-			public static final String timeStamp="Tue, 23-March-2010 16:23:22 CET";
+			public static final String timeStamp="Thu, 8-April-2010 14:40:57 CEST";
 			public static final String svnLog="svn: '.' is not a working copy";
 		}
 	
