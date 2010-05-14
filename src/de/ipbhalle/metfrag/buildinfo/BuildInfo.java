fdf7d8d16e3d61418813765fbc15f50220230098
Sebastian Wolf <swolf@ipb-halle.de>
2010-05-14 12:49:08 +0200

    just commit buildinfo

--- src/de/ipbhalle/metfrag/buildinfo/BuildInfo.java
+++ src/de/ipbhalle/metfrag/buildinfo/BuildInfo.java
@@ -2,7 +2,7 @@
 		package de.ipbhalle.metfrag.buildinfo;
 		public class BuildInfo {
 			public static final String revisionNumber="exported";
-			public static final String timeStamp="Thu, 8-April-2010 14:40:57 CEST";
+			public static final String timeStamp="Fri, 14-May-2010 12:47:49 CEST";
 			public static final String svnLog="svn: '.' is not a working copy";
 		}
 	
