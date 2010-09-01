4a0084d7be862a781f771cfd2bafb1cca24dc649
Sebastian Wolf <swolf@ipb-halle.de>
2010-09-01 10:08:08 +0200

    added Constants class

---
+++ src/de/ipbhalle/metfrag/tools/Constants.java
@@ -0,0 +1,8 @@
+package de.ipbhalle.metfrag.tools;
+
+public class Constants {
+	
+	public static final double PROTON_MASS = 1.007276;
+	public static final double HYDROGEN_MASS = MolecularFormulaTools.getMonoisotopicMass("H1");
+
+}
