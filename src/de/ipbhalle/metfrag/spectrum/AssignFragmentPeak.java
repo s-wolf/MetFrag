36477664cec759cc40e38f2f3fb7c5a10e1efb77
Sebastian Wolf <swolf@ipb-halle.de>
2011-01-25 15:08:11 +0100

    Fixed bug!

--- src/de/ipbhalle/metfrag/spectrum/AssignFragmentPeak.java
+++ src/de/ipbhalle/metfrag/spectrum/AssignFragmentPeak.java
@@ -195,7 +195,7 @@ public class AssignFragmentPeak {
     					hydrogenCountString = Integer.toString(i - 1);
     				
     				//positive mode...no hydrogen added for i == 1
-    				else if(mode == 1 && i == 1)
+    				if(mode == 1 && i == 1)
     				{
     					//reduce the hydrogen
         				if(this.html)
