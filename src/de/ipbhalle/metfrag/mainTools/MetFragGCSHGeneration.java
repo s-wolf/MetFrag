67f6f7a889c9f94773ed18e7f7d8133a3aa2c1f2
Sebastian Wolf <swolf@ipb-halle.de>
2011-12-09 17:26:20 +0100

    added file

---
+++ src/de/ipbhalle/metfrag/mainTools/MetFragGCSHGeneration.java
@@ -0,0 +1,89 @@
+package de.ipbhalle.metfrag.mainTools;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+
+import de.ipbhalle.metfrag.spectrum.WrapperSpectrum;
+
+public class MetFragGCSHGeneration {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		//"/home/swolf/MOPAC/BATCH/jar/PreprocessMolecules.jar" "/home/swolf/MOPAC/ProofOfConcept/pubchem/" "/home/swolf/MOPAC/BATCH/sh/" 600 600
+		
+//		String writePath = "/home/swolf/MOPAC/BondOrderTests/Hill_ProofOfConcept/MFsh/";
+//		String pathToSpectra = "/home/swolf/MOPAC/BondOrderTests/Hill_ProofOfConcept/testData/";
+//		String pathToJar = "/home/swolf/MOPAC/BondOrderTests/Hill_ProofOfConcept/jar/MetFragPreCalculated.jar";
+//		String pathToPreCalculatedCML = "/home/swolf/MOPAC/BondOrderTests/Hill_ProofOfConcept/mopac_2400/";
+		
+		String writePath = "/home/swolf/MOPAC/100spec_SDF_GC-EIMS/10ppmshMF/";
+		String pathToSpectra = "/home/swolf/MOPAC/100spec_SDF_GC-EIMS/spectra/";
+		String pathToJar = "/home/swolf/MOPAC/100spec_SDF_GC-EIMS/jar/MetFragPreCalculatedGC_10ppm.jar";
+		String pathToPreCalculatedCML = "/home/swolf/MOPAC/100spec_SDF_GC-EIMS/sdf_calculated/";
+		String configPath = "/home/swolf/MOPAC/100spec_SDF_GC-EIMS/config/";
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+	    java.util.Date date = new java.util.Date();
+		String dateString = dateFormat.format(date);
+		
+		if(!writePath.contains("shMF/"))
+		{
+			System.err.println("Please use a folder structure that uses /.../.../sh/ at the end");
+			System.exit(1);
+		}
+		
+		//loop over all files in folder
+		FileUtils.deleteDirectory(new File(writePath));
+		new File(writePath).mkdirs();
+		
+		Map<String, File> candidateFolderMap = new HashMap<String, File>(); 
+		File[] candidateFolder = new File(pathToPreCalculatedCML).listFiles();
+		Arrays.sort(candidateFolder);
+		for (File file : candidateFolder) {
+			candidateFolderMap.put(file.getName().split("_")[0], file);
+		}
+		
+		
+		File[] spectraFolder = new File(pathToSpectra).listFiles();
+		Arrays.sort(spectraFolder);
+		for (File file : spectraFolder) {
+			
+			if(!file.isFile())
+				continue;
+			
+			String fileName = file.getName();
+			int dotPos = fileName.indexOf(".");
+			String extension = "";
+			if(dotPos >= 0)
+				extension = fileName.substring(dotPos);
+			
+			
+			File f2 = new File(writePath + "sge_" + file.getName().split("\\.")[0] + ".sh"); 
+			
+			String number = file.getName().split("_")[1].split("\\.")[0].substring(2);
+			
+			BufferedWriter out = new BufferedWriter(new FileWriter(f2));
+			out.write("#!/bin/bash");
+			out.newLine();
+			//CID_3365_spectrum.txt 2011-02-02_16-00-00 /home/swolf/MOPAC/Hill_PubChem_Formula/pubchemClusteredMopac/mopac_600/C30H60N3O3/ 1
+//			out.write("java -Dproperty.file.path=" + pathToSpectra + "config/" + " -Xms1500m -Xmx5500m -jar " + pathToJar + " \"" + file.getPath() + "\" \"" + dateString + "\" \"" + new File(pathToPreCalculatedCML) + "/" + spectrum.getFormula().trim() + "/\"");
+//			"/home/swolf/MOPAC/100spec_SDF_GC-EIMS/spectra/RandSel100Third_00013.mf" "2011-11-29_13-33-33" "/home/swolf/MOPAC/100spec_SDF_GC-EIMS/sdf_calculated/013_38120_C1H5Si1Br1/"
+			out.write("java -Dproperty.file.path=" + configPath + " -Xms1500m -Xmx5500m -jar " + pathToJar + " \"" + file.getPath() + "\" \"" + dateString + "\" \"" + candidateFolderMap.get(number) + "/\"");
+		  	out.close();
+
+		}
+	}
+}
